3807cab0287fb861725d250d6f39ce67150b97b1
SkyTu <1336923451@qq.com>
2021-10-30 19:27:55 +0800

    connection of OT updated

--- OTextension/Server/sender.cpp
+++ OTextension/Server/sender.cpp
@@ -1,83 +1,116 @@
 /*
  * @Author: your name
  * @Date: 2021-10-20 12:49:11
- * @LastEditTime: 2021-10-21 14:31:24
+ * @LastEditTime: 2021-10-30 16:13:43
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /txy/PaperReproduce/OTextension/reciever.cpp
  */
-#include<iostream>
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <stdlib.h>
+#include <iostream>
+#include <cstdio>
 #include <cstring>
-#include <arpa/inet.h>
-#include <unistd.h>
+#include <cstdlib>
+#include <sys/socket.h>
+#include <sys/unistd.h>
+#include <sys/types.h>
+#include <sys/errno.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <string>
 #include <vector>
 #include "../PrimeGroup.hpp"
 #define addr "10.176.34.170" 
+#define MAXLINK 1024
+#define BUFFSIZE 1024
+#define DEFAULT_PORT 16555
+
+
+int sockfd, connfd;    // 定义服务端套接字和客户端套接字
+
+void stopServerRunning(int p)
+{
+    close(sockfd);
+    printf("Close Server\n");
+    exit(0);
+}
 int main()
 {
-    int sockfd = -1;
- 
-    sockfd = socket( AF_INET, SOCK_STREAM, 0 );
-    if ( -1 == sockfd ) {
-        perror( "sock created" );
-        exit( -1 );
+    struct sockaddr_in servaddr;    // 用于存放ip和端口的结构
+    char buff[BUFFSIZE];    // 用于收发数据
+    // 对应伪代码中的sockfd = socket();
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (-1 == sockfd)
+    {
+        printf("Create socket error(%d): %s\n", errno, strerror(errno));
+        return -1;
     }
- 
-    struct sockaddr_in server;
-    memset(&server,0,sizeof(struct sockaddr_in));
-    server.sin_addr.s_addr = inet_addr(addr);
-    server.sin_port = htons(7777);
-    server.sin_family = AF_INET;
- 
-    int res = -1;
-    res = connect( sockfd, (struct sockaddr*)&server, sizeof( server ) );
-    if( -1 == res ){
-        perror( "sock connect" );
-        exit( -1 );
+    // END
+    // 对应伪代码中的bind(sockfd, ip::port和一些配置);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(DEFAULT_PORT);
+    if (-1 == bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)))
+    {
+        printf("Bind error(%d): %s\n", errno, strerror(errno));
+        return -1;
     }
- 
-    char sendBuf[1024] = { 0 };
-    char recvBuf[1024] = { 0 };
-    while(true) {
-        printf("Oblivious Transfer Begins! Please input the message you want to transport\n");
+    // END
+    // 对应伪代码中的listen(sockfd);    
+    if (-1 == listen(sockfd, MAXLINK))
+    {
+        printf("Listen error(%d): %s\n", errno, strerror(errno));
+        return -1;
+    }
+    // END
+    printf("Listening...\n");
+    while (true)
+    {
+        signal(SIGINT, stopServerRunning);    // 这句用于在输入Ctrl+C的时候关闭服务器
+        // 对应伪代码中的connfd = accept(sockfd);
+        connfd = accept(sockfd, NULL, NULL);
+        if (-1 == connfd)
+        {
+            printf("Accept error(%d): %s\n", errno, strerror(errno));
+            return -1;
+        }
+
+        //变量声明
         std::string m0, m1;
+        PrimeGroup p;
+        int64 c;
+        //生成随机数C
+        p.initC();
+        std::vector<int64> PK;
+
+        //开始传输数据
+        printf("Oblivious Transfer Begins! Please input the message you want to transport\n");
         std::cin>>m0;
         std::cin>>m1;
         
-        PrimeGroup p;
-        p.initC();
+        
         srand(time(0));
         int k = rand()%p.cyc_group.size()+1;//生成随机数1<=k<=q(q为循环群的阶)
-        std::vector<int64> PK;
         PK.push_back(p.cyc_group[k+1]);//获得PK[0]，即为g的k次方
         PK.push_back(p.divide(p.c,PK[0]));//获得PK[1]
+        c = p.c;
+        bzero(buff, BUFFSIZE);
+        strcpy(buff, std::to_string(c).c_str());
+        send(connfd, buff, strlen(buff), 0);
+        printf("Send: c = %s\n", buff);
+
+        bzero(buff, BUFFSIZE);
+        strcpy(buff, std::to_string(PK[0]).c_str());
+        send(connfd, buff, strlen(buff), 0);
+        printf("Send: PK[0] = %s\n", buff);
         
-        std::string c = std::to_string(p.c);
-        memset(sendBuf, 0, 1024);
-        strcpy(sendBuf, c.c_str());
-        
-        send(sockfd, sendBuf, sizeof( sendBuf ),0);//传递随机数C
-        std::cout<<sendBuf<<std::endl;
-        srand(time(0));
-        std::string PKrandom = std::to_string(PK[rand()%2]);//随机选取一个进行传输
-        memset(sendBuf, 0, 1024);
-        strcpy(sendBuf, PKrandom.c_str());
-        std::cout<<sendBuf<<std::endl;
-        
-        send( sockfd, sendBuf, sizeof( sendBuf ),0);
-        recv( sockfd, recvBuf, sizeof( recvBuf ),0);
-        fputs( recvBuf, stdout );
-        memset( sendBuf, 0, sizeof( sendBuf ) );
-        memset( recvBuf, 0, sizeof( recvBuf ) );
+        bzero(buff, BUFFSIZE);
+        // 对应伪代码中的recv(connfd, buff);
+        recv(connfd, buff, BUFFSIZE - 1, 0);
+        printf("Recv: %s\n", buff);
+    	    
     }
- 
-    close( sockfd );
- 
+    close(connfd);
+    // END
     return 0;
 }
-
-    
